933fe005d7cc691734d7050641c5a5dc2f7bd966
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-08 14:09:36 -0500

    first commit

---
+++ src/main/java/com/ecommercecontactservice/EcommerceContactServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercecontactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceContactServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceContactServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommercecontactservice/EcommerceContactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercecontactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceContactServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
